21266d7d2cffcdb3f9dd32b6cd881d9d6a2672dc
Ebun Egbewunmi <ebunegbewunmi@gmail.com>
2025-12-03 21:12:10 -0600

    types

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export type ElementType = "image" | "text" | "swatch";
+
+export interface BaseElement {
+  id: string;
+  type: ElementType;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rotation: number;
+  zIndex: number;
+}
+
+export interface ImageElement extends BaseElement {
+  type: "image";
+  src: string;
+}
+
+export interface TextElement extends BaseElement {
+  type: "text";
+  text: string;
+  color: string;
+  fontSize: number;
+}
+
+export interface SwatchElement extends BaseElement {
+  type: "swatch";
+  color: string;
+}
+
+export type MoodboardElement = ImageElement | TextElement | SwatchElement;
